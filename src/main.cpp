9c9683802c5d6905968a3a6e7e774844ce088f6a
Deborah Macedo <deborah.macedo.104.ufrn.edu.br>
2025-04-27 22:27:38 -0300

    fix: doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,22 @@
 #include <string>
 #include <string_view>
 
-#include "enums/CharGroupEnum.h"
-#include "PassgenUtils.h"
-#include "zxcvbn-c/zxcvbn.h"
+#include "../include/enums/CharGroupEnum.h"
+#include "../include/PassgenUtils.h"
+#include "../include/zxcvbn-c/zxcvbn.h"
 
 //== Some default values.
 constexpr size_t default_pass_length{ 7 };
 
 PassgenUtils pu{};
+
+/**
+ * @brief Stores options selected by the user.
+ *
+ * This structure keeps track of the password generation settings such as
+ * password length, whether to display password strength, and the 
+ * selected character groups.
+ */
 
 /// This struct stores the options passed in via CLI.
 struct RunningOptions {
@@ -19,6 +27,15 @@ struct RunningOptions {
   std::array<bool, GROUPS_AMOUNT> activeGroups{};
   int activeGroupsCounter{ 0 };
 };
+
+/**
+ * @brief Fuction that shows help to the user. 
+ * Shows each one of the groups available
+ *
+ * It shows a massage of help, the user can see arguments, e the right way of write an input
+ * Can be called when there is an error or the user asks for help.
+ *
+ */
 
 /// Show help screen and error message
 void usage(std::string_view msg = "") {
@@ -43,6 +60,20 @@ void usage(std::string_view msg = "") {
                "  -t, --strength     Show password strength classification.\n"
                "  -h, --help         Show this help screen.\n";
 }
+
+/**
+ * @brief Gets the user input and validates it.
+ *
+ * Parses the command-line arguments provided by the user and updates
+ * the RunningOptions structure. Then validate the argumentsoptions like help,
+ * password length, password strength analysis, and active character groups.
+ *
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line argument strings.
+ * @param run_options Struct where the parsed options will be stored.
+ */
+
+
 
 /// Validates and parses the command line arguments
 void validate_arguments(int argc, char* argv[], RunningOptions& run_options) {
@@ -100,6 +131,18 @@ void validate_arguments(int argc, char* argv[], RunningOptions& run_options) {
   }
 }
 
+/**
+ * @brief Generates a password based on the provided options.
+ *
+ * Iterates through the active character groups and appends randomly selected characters 
+ * to build a password of the desired length. Ensures a balanced distribution of character types.
+ * Finally, shuffles the resulting password for added randomness.
+ *
+ * @param run_options Struct containing the user options for the generation of the password.
+ * @return The generated password.
+ */
+
+
 std::string generate_password(const RunningOptions& run_options) {
   std::string password{};
   size_t charsPerGroup{run_options.pass_length / run_options.activeGroupsCounter};
@@ -127,6 +170,17 @@ std::string generate_password(const RunningOptions& run_options) {
   return password;
 }
 
+/**
+ * @brief Evaluates the strength of a given password.
+ *
+ * Calculates the password's entropy using the zxcvbn-c library and 
+ * returns a strenght classification of the password.
+ *
+ * @param password The password to evaluate.
+ * @return A string indicating the password's strength.
+ */
+
+
 std::string password_quality(std::string password) {
   double entropy = ZxcvbnMatch(password.c_str(), nullptr, nullptr);
 
@@ -144,6 +198,18 @@ std::string password_quality(std::string password) {
 
   return "Excellent";
 }
+
+/**
+ * @brief The main function of the password generation program.
+ *
+ * This program validates the command line arguments, generates a password 
+ * according to the provided options, and optionally displays the password's strength.
+ *
+ * @param argc Number of command line arguments.
+ * @param argv Array of strings containing the arguments.
+ * @return Program exit code (EXIT_SUCCESS on success).
+ */
+
 
 int main(int argc, char* argv[]) {
   RunningOptions run_options;  // Stores incoming arguments.
